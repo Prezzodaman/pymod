5b87df4b47dcf0a64b8b149aa7c89509a5b2ba84
Didier Malenfant <didier@malenfant.net>
2024-05-15 21:56:05 +0200

    Added standard GPL file header
    And fixed the license type for pypi

--- pymod/__about__.py
+++ pymod/__about__.py
@@ -1,8 +1,18 @@
 #
 # Copyright (c) 2023-present Presley Peters (Prezzo).
-# Pymod is licensed under GPL v3.0.
 #
 # This file is part of pymod.
+#
+# pymod is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pymod is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+# License for more details.
+#
+# You should have received a copy of the GNU General Public License along with pymod. If not,
+# see <https://www.gnu.org/licenses/>.
 #
 
 __version__ = '1.0.1'
